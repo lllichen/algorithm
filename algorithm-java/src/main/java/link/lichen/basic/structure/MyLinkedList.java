79322260e5278fd2d6e4c53e9be9b5fdc3cf70d9
lichen <lichen@daojia.com>
2018-04-09 10:28:54 +0800

    add linkList

---
+++ algorithm-java/src/main/java/link/lichen/basic/structure/MyLinkedList.java
@@ -0,0 +1,18 @@
+package link.lichen.basic.structure;
+
+import java.util.Iterator;
+
+/**
+ * Created by lichen@daojia.com on 2018-4-9.
+ */
+public class MyLinkedList implements Iterable {
+
+
+
+
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
