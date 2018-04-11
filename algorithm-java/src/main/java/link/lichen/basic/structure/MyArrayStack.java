841d4926f2450461cdec281931e76e8b84cadcda
lichen <lichen@daojia.com>
2018-04-11 18:38:50 +0800

    add stack

---
+++ algorithm-java/src/main/java/link/lichen/basic/structure/MyArrayStack.java
@@ -0,0 +1,69 @@
+package link.lichen.basic.structure;
+
+/**
+ * Created by lichen@daojia.com on 2018-4-11.
+ */
+public class MyArrayStack<T> {
+
+
+    private T [] theArray;
+
+    private int DEFAULT_SIZE = 10;
+
+    private int theSize;
+
+    private int topOfStack;
+
+    public MyArrayStack(){
+        doClear();
+    }
+
+    private void doClear() {
+        theSize = 0;
+        ensureCapacity(DEFAULT_SIZE);
+        topOfStack = -1;
+    }
+
+    public int size(){
+        return theSize;
+    }
+
+    public boolean isEmpty(){
+        return topOfStack == -1;
+    }
+
+
+    private void ensureCapacity(int newCapacity) {
+        if (newCapacity < theSize){
+            return;
+        }
+        T[] old = theArray;
+        theArray = (T[]) new Object[newCapacity];
+        for (int i = 0 ; i < size(); i++)
+            theArray[i] = old[i];
+    }
+
+    public void push(T t){
+        if (size() == theArray.length){
+            ensureCapacity( 2*size()+1 );
+        }
+        theSize++;
+        theArray[++topOfStack] = t;
+    }
+
+    public T pop(){
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException(  );
+        }
+        theSize--;
+        return theArray[topOfStack--];
+    }
+
+    public T top(){
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException(  );
+        }
+        return theArray[topOfStack];
+    }
+
+}
---
+++ algorithm-java/src/test/java/ink/lichen/basic/structure/MyArrayStackTest.java
@@ -0,0 +1,57 @@
+package ink.lichen.basic.structure;
+
+import link.lichen.basic.structure.MyArrayStack;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by lichen@daojia.com on 2018-4-11.
+ */
+public class MyArrayStackTest {
+
+    @Test
+    public void testStackPushAndPop(){
+        MyArrayStack<String> stack = new MyArrayStack<>();
+        stack.push( "a" );
+        stack.push( "b" );
+        stack.push( "c" );
+        stack.push( "d" );
+
+        Assert.assertTrue( stack.pop().equals( "d" ) );
+        Assert.assertTrue( stack.size()==3 );
+        Assert.assertTrue( stack.pop().equals( "c" ) );
+        Assert.assertTrue( stack.size()==2 );
+        Assert.assertTrue( stack.pop().equals( "b" ) );
+        Assert.assertTrue( stack.size()==1 );
+        Assert.assertTrue( stack.pop().equals( "a" ) );
+        Assert.assertTrue( stack.size()==0 );
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testEmptyStackPop(){
+        MyArrayStack<String> stack = new MyArrayStack<>();
+        Assert.assertTrue( stack.pop() == null);
+    }
+
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testEmptyStackTop(){
+        MyArrayStack<String> stack = new MyArrayStack<>();
+        Assert.assertTrue( stack.pop() == null);
+    }
+
+
+
+
+    @Test
+    public void testStackClear(){
+        MyArrayStack<String> stack = new MyArrayStack<>();
+        stack.push( "a" );
+        stack.push( "b" );
+        stack.push( "c" );
+        stack.push( "d" );
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+    }
+
+}
