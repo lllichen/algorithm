615bf0a1b23970d1cc667e668c000c1a95a8ed51
lichen <lichen@daojia.com>
2018-04-14 10:51:25 +0800

    add queue

---
+++ algorithm-java/src/main/java/link/lichen/basic/structure/MyArrayQueue.java
@@ -0,0 +1,34 @@
+package link.lichen.basic.structure;
+
+/**
+ * Created by lichen@daojia.com on 2018-4-13.
+ */
+public class MyArrayQueue<T> {
+
+    private T [] theArray;
+
+    private int theSize;
+
+    private int front;
+
+    private  int back;
+
+
+    public MyArrayQueue(){
+        doClear();
+    }
+
+    private void doClear() {
+        front = back = 0;
+        theSize = 0;
+//        En
+    }
+
+
+    //
+
+    public void enqueue(T val){
+        
+    }
+
+}
