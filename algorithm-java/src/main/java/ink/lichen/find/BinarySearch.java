40fc3fdd1c333b9a03d46573a0f6251a2143feb1
lichen <lichen@daojia.com>
2018-04-25 15:06:16 +0800

    add code for second

--- algorithm-java/src/main/java/ink/lichen/find/BinarySearch.java
+++ algorithm-java/src/main/java/ink/lichen/find/BinarySearch.java
@@ -6,36 +6,38 @@ package ink.lichen.find;
 public class BinarySearch {
 
 
-    // -1 error
-    public static int find(int [] a,int begin, int end,int dest){
-
-        if(begin > end){
+    public static final int find(int[] a,int val){
+        if (a.length < 0) {
             return -1;
         }
-        int mid =(begin+end)/2;
-        if (a[mid]<dest)
-            return find( a,mid+1,end,dest );
-        else if (a[mid] > dest)
-            return find( a,begin,mid-1,dest );
-        else
-            return mid+1;
-    }
+        int high = a.length-1;
+        int low = 0;
 
-
-    public static int find(int[] a,int target){
-        int begin = 0;
-        int end = a.length-1;
-        while ( begin <= end){
-            int mid = (begin+end)/2;
-            if (a[mid] > target ){
-                end = mid-1;
-            }else if (a[mid] < target){
-                begin = mid+1;
-            }else if (a[mid] == target){
-                 return mid+1;
+        while (low <= high) {
+            int mid = (high+low)/2;
+            if (a[mid] > val ){
+                high = mid -1;
+            }else if(a[mid] < val ){
+                low = mid + 1;
+            }else{
+                return mid;
             }
         }
         return -1;
     }
 
+
+    public static final int find(int[] a, int val, int low, int high){
+        if (low > high){
+            return -1;
+        }
+        int mid = (high+low)/2;
+        if (a[mid] > val){
+            return find( a,val,low,mid-1 );
+        }else if (a[mid] < val){
+            return find( a,val,mid+1,high );
+        }else {
+            return mid+1;
+        }
+    }
 }
--- algorithm-java/src/test/java/ink/lichen/basic/find/BinarySearchTest.java
+++ algorithm-java/src/test/java/ink/lichen/basic/find/BinarySearchTest.java
@@ -12,12 +12,12 @@ public class BinarySearchTest {
     @Test
     public void testSearchWhile(){
         int[] ints = new int[]{1,2,3,4,5,6,7,8,9,10,11,15,20,22,30,31};
-        System.out.println(BinarySearch.find(ints,11));
+        System.out.println(BinarySearch.find(ints,32));
     }
 
     @Test
     public void testSearchIterator(){
         int[] ints = new int[]{1,2,3,4,5,6,7,8,9,10,11,15,20,22,30,31};
-        System.out.println(BinarySearch.find(ints,0,ints.length-1,15));
+        System.out.println(BinarySearch.find(ints,15,0,ints.length-1));
     }
 }
