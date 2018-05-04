bd95609986fdc19b7d51124e36112492e5e6cb24
lichen <lichen@daojia.com>
2018-05-04 13:52:19 +0800

    add bufferTest

---
+++ common-java/src/main/java/ink/lichen/netty/ByteBufferTest.java
@@ -0,0 +1,28 @@
+package ink.lichen.netty;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by lichen@daojia.com on 2018-4-26.
+ */
+public class ByteBufferTest {
+
+    public static void main(String[] args) {
+//        ByteBuffer byteBuffer = ByteBuffer.allocate( 256 );
+//
+//        byte b = byteBuffer.get( 0 );
+//
+//        b = (1 << 7)-1;
+//
+//        System.out.println(b);
+//        System.out.println((char)(b));
+//
+//        System.out.println(byteBuffer.get( 0 ));
+
+        int a = 13;
+        System.out.println(a<<2);
+
+    }
+}
+
+
