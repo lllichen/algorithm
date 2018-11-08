106a30afff94a0e4a4188c6a5a4bbe2ade61869d
lichen <lichen@daojia-inc.com>
2018-11-08 15:39:26 +0800

    add find

--- algorithm-java/src/main/java/ink/lichen/find/QuickSort.java
+++ algorithm-java/src/main/java/ink/lichen/find/QuickSort.java
@@ -4,4 +4,14 @@ package ink.lichen.find;
  * Created by lichen@daojia.com on 2018-11-8.
  */
 public class QuickSort {
+
+
+    public static <T extends Comparable<? super T>> void quicksort(T[] a){
+        int n = a.length,lo = 0,hi = n-1;
+        quicksort(a,lo,hi);
+    }
+
+    public static  <T extends Comparable<? super T>> void quicksort(T[] a,int lo,int hi){
+
+    }
 }
