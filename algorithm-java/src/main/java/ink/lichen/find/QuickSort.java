a5b0bf11fa2a4fc624f69630e0edb778ba109ab1
lichen <lichen@daojia-inc.com>
2018-11-08 16:07:24 +0800

    add quickSort

--- algorithm-java/src/main/java/ink/lichen/find/QuickSort.java
+++ algorithm-java/src/main/java/ink/lichen/find/QuickSort.java
@@ -11,7 +11,19 @@ public class QuickSort {
         quicksort(a,lo,hi);
     }
 
-    public static  <T extends Comparable<? super T>> void quicksort(T[] a,int lo,int hi){
+    private static  <T extends Comparable<? super T>> void quicksort(T[] a,int lo,int hi){
 
     }
+
+    private static <T extends Comparable<? super  T>> T median3(T[] a, int left,int right){
+
+        int center = (left + right) / 2;
+        if (a[center].compareTo(a[left]) < 0){
+//            swapReference();
+        }
+
+        return a[0];
+    }
+
+//    private void swapReference
 }
