442abf158bd13cf915f1b61e81ce5d37d7afea0c
lichen <lichen@daojia-inc.com>
2018-11-20 20:32:38 +0800

    add BinaryHeap

---
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/BinaryHeap.java
@@ -0,0 +1,12 @@
+package ink.lichen.basic.structure;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-20.
+ */
+public class BinaryHeap<T extends Comparable<? super T>> {
+
+
+    public BinaryHeap(){
+
+    }
+}
