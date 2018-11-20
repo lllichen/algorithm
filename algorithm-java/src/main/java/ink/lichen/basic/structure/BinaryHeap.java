8dff8d5fa3c3053e4a92c582bc2adf968a79132d
lichen <lichen@daojia-inc.com>
2018-11-20 20:46:45 +0800

    add BinaryHeap

--- algorithm-java/src/main/java/ink/lichen/basic/structure/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/BinaryHeap.java
@@ -5,8 +5,67 @@ package ink.lichen.basic.structure;
  */
 public class BinaryHeap<T extends Comparable<? super T>> {
 
+    private static final int DEFAULT_CAPACITY = 16;
+
+    private int currentSize;
+
+    private T [] array;
 
     public BinaryHeap(){
 
     }
+
+    public void insert(T t){
+        if (currentSize == array.length-1){
+            enlargeArray(array.length*2 + 1);
+        }
+        int hole = ++currentSize;
+        for (array[0] = t; t.compareTo(array[hole/2]) < 0; hole /= 2){
+            array[hole] = array[hole/2];
+        }
+        array[hole] = t;
+    }
+
+    private void  enlargeArray(int newSize){
+
+
+
+    }
+
+    public  T deleteMin(){
+        if (isEmpty()){
+            throw new UnderflowException();
+        }
+        T min = findMin();
+        array[1] = array[currentSize--];
+        percolateDown(1);
+        return min;
+    }
+
+    private void percolateDown(int hole) {
+        int child;
+        T tmp = array[hole];
+        for (;hole*2 <= currentSize; hole = child){
+            child = hole * 2;
+            if ( child != currentSize && array[child + 1].compareTo(array[child])<0){
+                child++;
+            }
+            if (array[child].compareTo(tmp) < 0){
+                array[hole] = array[child];
+            }else
+                break;
+        }
+        array[hole] = tmp;
+    }
+
+    private T findMin() {
+
+        return array[1];
+    }
+
+    private boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+
 }
---
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/UnderflowException.java
@@ -0,0 +1,7 @@
+package ink.lichen.basic.structure;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-20.
+ */
+public class UnderflowException extends RuntimeException {
+}
