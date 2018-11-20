65cef4d711631839ca22e2b19d7453bb3d8930bc
lichen <togetherlichen@gmail.com>
2018-11-21 07:58:30 +0800

    add heapSort

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test_1/Sort.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test_1/Sort.java
@@ -4,6 +4,12 @@ public class Sort {
 
 
     public static <T extends Comparable<? super T>> void heapSort(T [] items){
+        for (int i = items.length/2 -1; i>= 0; i--){
+            percDown(items,i,items.length);
+        }
+    }
+
+    private static<T extends Comparable< ? super T>s> void percDown(T[] items, int i, int hole) {
 
     }
 }
