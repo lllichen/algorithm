4d40eb131ccf39735cc9660900c1af9a664c34cb
lichen <lichen@daojia-inc.com>
2018-11-21 20:56:50 +0800

    add Sort

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test_2/Sort.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test_2/Sort.java
@@ -53,11 +53,52 @@ public class Sort {
         }
     }
 
+    public static <T extends Comparable<? super T>> void mergeSort(T[] a){
+        T[] temp = (T[]) new Comparable[a.length];
+        mergeSort(a,temp,0,a.length-1);
+    }
+
+    private static <T extends Comparable<? super T>> void mergeSort(T[] a, T[] temp, int left, int right) {
+        if (left < right){
+            int center = (left+right)>>1;
+            mergeSort(a,temp,left,center);
+            mergeSort(a,temp,center+1,right);
+            merge(a,temp,left,center+1,right);
+        }
+    }
+
+    private static<T extends Comparable<? super T>> void merge(T[] a, T[] temp, int leftPos, int rightPos, int rightEnd) {
+        int leftEnd = rightPos - 1;
+        int tempPos = leftPos;
+        int numElements = rightEnd - leftPos+1;
+
+        while (leftPos <= leftEnd && rightPos <= rightEnd){
+            if (a[leftPos].compareTo(a[rightPos]) <= 0){
+                temp[tempPos++] = a[leftPos++];
+            }else {
+                temp[tempPos++] = a[rightPos++];
+            }
+        }
+
+        while (leftPos <= leftEnd){
+            temp[tempPos++]=a[leftPos++];
+        }
+
+        while (rightPos <= rightEnd){
+            temp[tempPos++] = a[rightPos++];
+        }
+        for (int i=0; i < numElements; i++,rightEnd--){
+            a[rightEnd] = temp[rightEnd];
+        }
+    }
+
+
     public static void main(String[] args) {
 
         Integer a[] = new Integer[] {4,31,6,88,12,4,3,12,77,8,9,15,4,5,6};
-        heapSort(a);
-
+        //n*log(n)
+//        heapSort(a);
+        mergeSort(a);
         print(a);
     }
 }
