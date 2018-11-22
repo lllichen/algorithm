38589b7011cd358163f59f7ce04b02008093f3e0
lichen <lichen@daojia-inc.com>
2018-11-22 14:51:08 +0800

    add

---
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/DisjSets.java
@@ -0,0 +1,27 @@
+package ink.lichen.basic.structure;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-22.
+ */
+public class DisjSets {
+
+    private int[] s;
+
+    public DisjSets(int numElements){
+        s = new int[numElements];
+        for (int i = 0 ;i < s.length;i++){
+            s[i] = -1;
+        }
+    }
+
+    public void union(int root1,int root2){
+        s[root2] = root1;
+    }
+
+    public int find(int x){
+        if (s[x] < 0){
+            return x;
+        }else
+            return find(s[x]);
+    }
+}
