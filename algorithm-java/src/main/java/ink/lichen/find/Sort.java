35fc57ec1f8004df79e54246e7f84b58e76d9203
lichen <togetherlichen@gmail.com>
2018-11-25 10:15:14 +0800

    countRadixSort

--- algorithm-java/src/main/java/ink/lichen/find/Sort.java
+++ algorithm-java/src/main/java/ink/lichen/find/Sort.java
@@ -87,9 +87,44 @@ public class Sort {
             int idx = 0;
             for (ArrayList<String> thisBucket : buckets){
                 for (String s : thisBucket){
+                    System.out.println(s);
                     arr[idx++] = s;
                 }
                 thisBucket.clear();
+            }
+        }
+    }
+
+    public static void  countingRadixSort(String[] arr, int stringLen){
+        final  int BUCKETS = 256;
+
+        int N = arr.length;
+        String [] buffer = new String[N];
+
+        String[] in = arr;
+        String[] out = buffer;
+
+        for (int pos = stringLen-1; pos >=0 ;pos --)
+        {
+            int[] count = new int[BUCKETS+1];
+
+            for (int i = 0; i < N; i++){
+                count[in[i].charAt(i)+1]++;
+            }
+            for (int b= 1; b <= BUCKETS;b++){
+                count[b] += count[b-1];
+            }
+            for (int i =0; i<N;i++){
+                out[count[in[i].charAt(pos)]++] = in[i];
+            }
+            String[] tmp = in;
+            in = out;
+            out = tmp;
+        }
+
+        if (stringLen % 2 == 1){
+            for (int i =0; i < arr.length;i++){
+                out[i] = in[i];
             }
         }
     }
