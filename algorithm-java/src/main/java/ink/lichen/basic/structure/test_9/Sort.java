6351261f2ed17d589d5d1e2282facc055b6438dc
lichen <togetherlichen@gmail.com>
2018-12-02 23:50:30 +0800

    add info

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test_9/Sort.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test_9/Sort.java
@@ -1,5 +1,6 @@
 package ink.lichen.basic.structure.test_9;
 
+import static ink.lichen.basic.structure.Support.CUT_OFF;
 import static ink.lichen.basic.structure.Support.print;
 import static ink.lichen.basic.structure.Support.swap;
 
@@ -36,10 +37,47 @@ public class Sort {
         a[i] = t;
     }
 //
-//    private static <T extends Comparable<? super T>> void heapSort(T[] a){
-//
-//    }
-//
+    private static <T extends Comparable<? super T>> void quickSort(T[] a){
+        quickSort(a,0,a.length-1);
+    }
+
+    private static<T extends Comparable<? super T>>  void quickSort(T[] a, int left, int right) {
+        if (left + CUT_OFF <= right){
+            T pivot = median3(a,left,right);
+            int i = left, j = right-1;
+            for (;;){
+                while (a[++i].compareTo(pivot)<0){};
+                while (a[--j].compareTo(pivot)>0){};
+                if (i < j){
+                    swap(a,i,j);
+                }else
+                    break;
+            }
+            swap(a,i,right-1);
+            quickSort(a,left,i-1);
+            quickSort(a,i+1,right);
+        }else
+            insertionSort(a,left,right);
+
+    }
+
+    private static <T extends Comparable<? super T>> T median3(T[] a, int left, int right) {
+        int center = (left+right)>>1;
+        if (a[center].compareTo(a[left])<0){
+            swap(a,left,center);
+        }
+        if (a[right].compareTo(a[left])<0){
+            swap(a,left,right);
+        }
+        if (a[right].compareTo(a[center])<0){
+            swap(a,right,center);
+        }
+        swap(a,center,right-1);
+
+        return a[right-1];
+    }
+
+    //
     private static <T extends Comparable<? super T>> void mergeSort(T[] a){
         T[] tmp = (T[]) new Comparable[a.length];
         mergeSort(a,tmp,0,a.length-1);
@@ -93,8 +131,8 @@ public class Sort {
         //n*log(n)
 //        heapSort(a);
 //        insertionSort(a,0,a.length-1);
-        mergeSort(a);
-//        quickSort(a);
+//        mergeSort(a);
+        quickSort(a);
         print(a);
     }
 }
