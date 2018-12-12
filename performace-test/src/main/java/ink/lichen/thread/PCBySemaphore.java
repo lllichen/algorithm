5e4bab88ee6d796996e50008bc834eb032f1550d
lichen <lichen@daojia-inc.com>
2018-12-12 18:42:22 +0800

    add PCBySemaphore

--- performace-test/src/main/java/ink/lichen/thread/PCBySemaphore.java
+++ performace-test/src/main/java/ink/lichen/thread/PCBySemaphore.java
@@ -7,11 +7,83 @@ import java.util.concurrent.Semaphore;
  */
 public class PCBySemaphore {
 
-    private Semaphore empty = new Semaphore(3);
-    private Semaphore semaphore = new Semaphore(3);
-
+    public static void main(String[] args) {
+        Share s = new Share();
+        Semaphore semCon = new Semaphore(0);
+        Semaphore semPro = new Semaphore(1);
+        new ProducerB(s,semPro,semCon).start();
+        new ConsumerB(s,semPro,semCon).start();
+    }
 }
 
-class share{
+class Share{
 
+    private char c;
+
+    void setSharedChar(char c){
+        this.c = c;
+    }
+
+    char getSharedChar(){
+        return c;
+    }
 }
+
+class ProducerB extends Thread{
+
+    private final Share s;
+
+    private final Semaphore semPro, semCon;
+
+    public ProducerB(Share s, Semaphore semPro, Semaphore semCon) {
+        this.s = s;
+        this.semPro = semPro;
+        this.semCon = semCon;
+    }
+
+    @Override
+    public void run() {
+        for (char ch = 'A'; ch <= 'Z'; ch++){
+            try {
+                semPro.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            s.setSharedChar(ch);
+            System.out.println(ch + " produced by producer.");
+            semCon.release();
+        }
+    }
+}
+
+
+class ConsumerB extends Thread{
+
+    private final Share s;
+
+    private final Semaphore semPro, semCon;
+
+    public ConsumerB(Share s, Semaphore semPro, Semaphore semCon) {
+        this.s = s;
+        this.semPro = semPro;
+        this.semCon = semCon;
+    }
+
+    @Override
+    public void run() {
+        char ch;
+        do{
+            try {
+                semCon.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ch = s.getSharedChar();
+            System.out.println(ch + " consumed by consumer.");
+            semPro.release();
+        }while (ch != 'Z');
+    }
+}
+
+
+
