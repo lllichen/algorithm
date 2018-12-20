4ae005228693c62adbcbd571e532ca5689c6359c
lichen <lichen@daojia-inc.com>
2018-12-20 21:04:08 +0800

    add redBlackTree

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_20/RedBlackTree.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_20/RedBlackTree.java
@@ -31,6 +31,71 @@ public class RedBlackTree<T extends Comparable<? super T>> {
         header.left = header.right = nullNode;
     }
 
+
+    private int compare(T item, RedBlackNode<T> t) {
+        if (t == header) {
+            return 1;
+        } else
+            return item.compareTo(t.element);
+    }
+
+    public void insert(T item) {
+        current = parent = grand = header;
+        nullNode.element = item;
+        while (compare(item, current) != 0) {
+            great = grand;
+            grand = parent;
+            parent = current;
+            current = compare(item, current) < 0 ? current.left : current.right;
+
+            // Check if two red children; fix if so
+            if (current.left.color == RED && current.right.color == RED) {
+                handleReorient(item);
+            }
+        }
+    }
+
+    private void handleReorient(T item)
+    {
+        current.color = RED;
+        current.left.color = BLACK;
+        current.right.color = BLACK;
+
+        if (parent.color == RED){
+            grand.color = RED;
+            if(compare(item,grand)<0 != (compare(item,parent)<0)){
+                parent = rotate(item,great);
+            }
+            current = rotate(item,great);
+            current.color = BLACK;
+        }
+        header.right.color = BLACK;
+    }
+
+    private RedBlackNode<T> rotate(T item, RedBlackNode<T> parent) {
+        if (compare(item,parent) < 0)
+            return parent.left = compare(item,parent.left)<0 ?rotateWithLeftChild(parent.left):
+                    rotateWithRightChild(parent.left);
+        else
+           return parent.right = compare(item,parent.right)<0 ? rotateWithLeftChild( parent.right ):  // RL
+                   rotateWithRightChild( parent.right );  // RR
+    }
+
+    private RedBlackNode rotateWithLeftChild(RedBlackNode k2) {
+        RedBlackNode k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        return k1;
+    }
+
+    private RedBlackNode rotateWithRightChild(RedBlackNode k1) {
+        RedBlackNode k2 = k1.right;
+        k1.right = k2.left;
+        k2.left = k1;
+        return k2;
+    }
+
+
     private static class RedBlackNode<T>{
 
         public RedBlackNode(T theElement) {
