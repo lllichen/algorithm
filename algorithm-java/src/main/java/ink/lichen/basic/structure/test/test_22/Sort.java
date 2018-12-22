babb7db3662f4ca41116c97d6c41cee22a2eace9
lichen <lichen@daojia-inc.com>
2018-12-22 15:17:49 +0800

    fix error

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_22/Sort.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_22/Sort.java
@@ -4,4 +4,25 @@ package ink.lichen.basic.structure.test.test_22;
  * Created by lichen@daojia.com on 2018-12-22.
  */
 public class Sort {
+
+    private static final int leftChild(int i){
+        return i*2+1;
+    }
+
+    private static <T extends Comparable<? super T>> void heapSort(T[] a){
+    }
+
+    private static <T extends Comparable<? super T>> void mergeSort(T[] a){
+    }
+
+    private static <T extends Comparable<? super T>> void mergeSort(T[] a,int left, int right){
+
+    }
+
+
+
+    private static <T extends Comparable<? super T>> void quickSort(T[] a){
+    }
+    private static <T extends Comparable<? super T>> void insertionSort(T[] a){
+    }
 }
