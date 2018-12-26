4384578a5869bb2d1bae5e66a70f3867df27d6b4
lichen <lichen@daojia-inc.com>
2018-12-26 16:50:41 +0800

    add redblack tree

---
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_25/RedBlackTree.java
@@ -0,0 +1,11 @@
+package ink.lichen.basic.structure.test.test_25;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-26.
+ */
+public class RedBlackTree<T extends Comparable<? super T>> {
+
+    private static class RedBlackNode<T> {
+
+    }
+}
