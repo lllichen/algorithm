91fe40481bda284d928b90a0db78225afa45cd6f
lichen <lichen@daojia-inc.com>
2018-12-29 11:20:49 +0800

    fix error

--- algorithm-java/src/main/java/ink/lichen/basic/structure/RedBlackTree.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/RedBlackTree.java
@@ -279,40 +279,20 @@ public class RedBlackTree<AnyType extends Comparable<? super AnyType>>
     // Test program
     public static void main( String [ ] args )
     {
-        RedBlackTree redBlackTree = new RedBlackTree();
-        redBlackTree.insert(10);
-        redBlackTree.insert(85);
-        redBlackTree.insert(15);
-        redBlackTree.insert(70);
-        redBlackTree.insert(20);
-        redBlackTree.insert(60);
-        redBlackTree.insert(30);
-        redBlackTree.insert(50);
-        redBlackTree.insert(65);
-        redBlackTree.insert(80);
-        redBlackTree.insert(90);
-        redBlackTree.insert(40);
-        redBlackTree.insert(5);
-        redBlackTree.insert(55);
-        redBlackTree.insert(45);
-//        System.out.println(redBlackTree.findMax());
-//        System.out.println(redBlackTree.findMin());
-        redBlackTree.printTree();
+        RedBlackTree<Integer> t = new RedBlackTree<>( );
+        final int NUMS = 400000;
+        final int GAP  =  35461;
 
-//        RedBlackTree<Integer> t = new RedBlackTree<>( );
-//        final int NUMS = 400000;
-//        final int GAP  =  35461;
-//
-//        System.out.println( "Checking... (no more output means success)" );
-//
-//        for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
-//            t.insert( i );
-//
-//        if( t.findMin( ) != 1 || t.findMax( ) != NUMS - 1 )
-//            System.out.println( "FindMin or FindMax error!" );
-//
-//        for( int i = 1; i < NUMS; i++ )
-//            if( !t.contains( i ) )
-//                System.out.println( "Find error1!" );
+        System.out.println( "Checking... (no more output means success)" );
+
+        for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+            t.insert( i );
+
+        if( t.findMin( ) != 1 || t.findMax( ) != NUMS - 1 )
+            System.out.println( "FindMin or FindMax error!" );
+
+        for( int i = 1; i < NUMS; i++ )
+            if( !t.contains( i ) )
+                System.out.println( "Find error1!" );
     }
 }
--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_26/RedBlackTree.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_26/RedBlackTree.java
@@ -5,7 +5,50 @@ package ink.lichen.basic.structure.test.test_26;
  */
 public class RedBlackTree<T extends Comparable<? super T>> {
 
+    private static final int BLACK = 1;
+    private static final int RED = 0;
+
+    private RedBlackNode<T> header;
+    private RedBlackNode<T> nullNode;
+
+    private RedBlackNode<T> current;
+    private RedBlackNode<T> parent;
+    private RedBlackNode<T> grand;
+    private RedBlackNode<T> great;
+
+    public RedBlackTree( ) {
+        nullNode      = new RedBlackNode<>(null);
+        nullNode.left = nullNode.right = nullNode;
+        header        = new RedBlackNode<>(null);
+        header.left   = header.right = nullNode;
+    }
+
+    private int compare(T item, RedBlackNode<T> t){
+        if (t == header){
+            return 1;
+        }else
+            return item.compareTo(t.element);
+    }
+
     private static class RedBlackNode<T> {
 
+
+        public RedBlackNode(T theElement){
+            this(theElement,null,null);
+        }
+
+        T element;
+
+        RedBlackNode<T> left;
+        RedBlackNode<T> right;
+
+        int color;
+
+        public RedBlackNode(T theElement, RedBlackNode lt, RedBlackNode rt) {
+            element = theElement;
+            left = lt;
+            right = rt;
+            color = BLACK;
+        }
     }
 }
