5cb44f2abe058dcb5934d38a1270cd681a253d6d
lichen <lichen@daojia-inc.com>
2019-01-02 15:15:58 +0800

    add sort

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_27/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_27/BinaryHeap.java
@@ -81,4 +81,18 @@ public class BinaryHeap<T extends Comparable<? super T>> {
     private boolean isEmpty() {
         return currentSize == 0;
     }
+
+    public static void main( String [ ] args )
+    {
+        int numItems = 10000;
+        BinaryHeap<Integer> h = new BinaryHeap<>( );
+        int i = 37;
+
+        for( i = 37; i != 0; i = ( i + 37 ) % numItems )
+            h.insert( i );
+        for( i = 1; i < numItems; i++ )
+            if( h.deleteMin( ) != i )
+                System.out.println( "Oops! " + i );
+    }
+
 }
