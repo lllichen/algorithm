47d78189e57e379f58d383e079cb3ced54206040
lichen <lichen@daojia-inc.com>
2019-01-16 21:21:21 +0800

    add info

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_30/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_30/BinaryHeap.java
@@ -1,5 +1,7 @@
 package ink.lichen.basic.structure.test.test_30;
 
+import ink.lichen.basic.structure.UnderflowException;
+
 /**
  * Created by lichen@daojia.com on 2019-1-15.
  */
@@ -40,5 +42,56 @@ public class BinaryHeap <T extends Comparable<? super T>>{
         }
     }
 
+    public T findMin(){
+        if (isEmpty()){
+            throw new UnderflowException();
+        }
+        T min =array[1];
+        return min;
+    }
 
+    private boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+    public T deleteMin(){
+        if (isEmpty()){
+            throw new UnderflowException();
+        }
+        T min = findMin();
+        array[1] = array [currentSize--];
+        percolateDown(1);
+        return min;
+
+    }
+
+    private void percolateDown(int hole) {
+        int child;
+        T tmp = array[hole];
+        for (;hole<<1 <= currentSize; hole = child){
+            child = hole << 1;
+            if (child != currentSize && array[child+1].compareTo(array[child])<0){
+                child++;
+            }
+            if(array[child].compareTo(tmp)<0){
+                array[hole] = array[child];
+            } else
+                break;
+        }
+        array[hole] = tmp;
+    }
+
+
+    public static void main( String [ ] args )
+    {
+        int numItems = 10000;
+        BinaryHeap<Integer> h = new BinaryHeap<>( );
+        int i = 37;
+
+        for( i = 37; i != 0; i = ( i + 37 ) % numItems )
+            h.insert( i );
+        for( i = 1; i < numItems; i++ )
+            if( h.deleteMin( ) != i )
+                System.out.println( "Oops! " + i );
+    }
 }
