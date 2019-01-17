a5e984174dd9bd704dc42760711b2fcd972c5ed2
lichen <lichen@daojia-inc.com>
2019-01-17 12:34:33 +0800

    add info

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_30/Sort.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_30/Sort.java
@@ -1,6 +1,7 @@
 package ink.lichen.basic.structure.test.test_30;
 
 import static ink.lichen.basic.structure.Support.CUT_OFF;
+import static ink.lichen.basic.structure.Support.print;
 import static ink.lichen.basic.structure.Support.swap;
 
 /**
@@ -31,7 +32,7 @@ public class Sort {
             if ( child != n-1 && a[child+1].compareTo(a[child])>0) {
                 child++;
             }
-            if (a[child].compareTo(tmp)<0){
+            if (a[child].compareTo(tmp)>0){
                 a[i] = a[child] ;
             }else
                 break;
@@ -85,11 +86,57 @@ public class Sort {
 
     private static < T extends Comparable<? super T>> void quickSort(T[] a,int left,int right){
         if (left + CUT_OFF <= right){
-
-
+            T pivot = median3(a,left,right);
+            int i = 0 , j = right-1;
+            for (;;){
+                while (a[++i].compareTo(pivot)<0){};
+                while (a[--j].compareTo(pivot)>0){};
+                if (i < j){
+                    swap(a,i,j);
+                }else
+                    break;
+            }
+            swap(a,i,right-1);
+            quickSort(a,left,i-1);
+            quickSort(a,i+1,right);
+        }else {
+            insertionSort(a,left,right);
         }
     }
-    private static < T extends Comparable<? super T>> void insertionSort(T[] a,int left,int right){
 
+    private static <T extends Comparable<? super T>> T median3(T[] a, int left, int right) {
+        int center= (left + right) >> 1;
+        if (a[center].compareTo(a[left])<0){
+            swap(a,center,left);
+        }
+        if (a[right].compareTo(a[left])<0){
+            swap(a,right,left);
+        }
+        if (a[right].compareTo(a[center])<0){
+            swap(a,center,right);
+        }
+        swap(a,center,right-1);
+        return a[right-1];
+    }
+
+    private static < T extends Comparable<? super T>> void insertionSort(T[] a,int left,int right){
+        for (int i = left ; i <= right; i++){
+            T tmp = a[i];
+            int j;
+            for ( j = i ; j > left && tmp.compareTo(a[j-1])<0;j --){
+                a[j] = a[j-1];
+            }
+            a[j] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer a[] = new Integer[]{4, 31, 6, 88, 12, 4, 3, 12, 77, 8, 9, 15, 4, 5, 6, 99, 0, 1000};
+        //n*log(n)
+//        heapSort(a);
+        insertionSort(a,0,a.length-1);
+//        mergeSort(a);
+//        quickSort(a);
+        print(a);
     }
 }
