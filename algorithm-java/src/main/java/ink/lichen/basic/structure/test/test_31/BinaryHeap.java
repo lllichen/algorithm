456be3ca932fa567cdfafd369dc5899ad455c2b8
lichen <lichen@daojia-inc.com>
2019-01-21 16:30:36 +0800

    add BinaryHeap

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_31/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_31/BinaryHeap.java
@@ -3,5 +3,24 @@ package ink.lichen.basic.structure.test.test_31;
 /**
  * Created by lichen@daojia.com on 2019-1-21.
  */
-public class BinaryHeap {
+public class BinaryHeap<T extends Comparable<? super T>> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private T[] array;
+
+    private int currentSize;
+
+    public BinaryHeap(){
+        this(DEFAULT_CAPACITY);
+    }
+
+
+    public BinaryHeap(int capacity) {
+        currentSize = 0;
+        array = (T[]) new Comparable[capacity+1];
+    }
+
+
+
 }
