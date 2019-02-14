cca853d8184008f92864bc3aa14c816c8b0970cf
lichen <lichen@daojia-inc.com>
2019-02-14 12:04:33 +0800

    add Sort

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_35/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_35/BinaryHeap.java
@@ -1,23 +1,21 @@
-package ink.lichen.basic.structure.test.test_33;
-
+package ink.lichen.basic.structure.test.test_35;
 
 import ink.lichen.basic.structure.UnderflowException;
 
 /**
- * Created by lichen@daojia.com on 2019-2-4.
+ * Created by lichen@daojia.com on 2019-2-14.
  */
 public class BinaryHeap<T extends Comparable<? super T>> {
 
-    private static final int DEFAULT_CAPACITY = 10;
-
     private T[] array;
+
+    private static final int DEFAULT_CAPACITY = 10;
 
     private int currentSize;
 
-    BinaryHeap(){
+    public BinaryHeap(){
         this(DEFAULT_CAPACITY);
     }
-
 
     public BinaryHeap(int capacity) {
         currentSize = 0;
@@ -26,29 +24,21 @@ public class BinaryHeap<T extends Comparable<? super T>> {
 
     public void insert(T t){
         if (currentSize == array.length-1){
-            enlargeArray(array.length*2+1);
+            enlargeArray(2*array.length+1);
         }
         int hole = ++currentSize;
-        for (array[0] = t;t.compareTo(array[hole>>1]) <0; hole = hole>>1){
+        for (array[0] = t; array[hole>>1].compareTo(t)>0;hole >>= 1){
             array[hole] = array[hole>>1];
         }
         array[hole] = t;
-    }
-
-    private void enlargeArray(int size) {
-        T[] old = array;
-        array = (T[]) new Comparable[size];
-        for (int i = 0 ; i < old.length; i++){
-            array[i] = old[i];
-        }
     }
 
     public T findMin(){
         if (isEmpty()){
             throw new UnderflowException();
         }
-        T min = array[1];
-        return min;
+        T mind = array[1];
+        return mind;
     }
 
     public T deleteMin(){
@@ -63,24 +53,34 @@ public class BinaryHeap<T extends Comparable<? super T>> {
 
     private void percolateDown(int hole) {
         int child;
-        T tmp = array[hole];
-        for (;hole<<1 <= currentSize; hole = child){
+        T t = array[hole];
+        for (; hole << 1 <= currentSize ; hole = child ){
             child = hole << 1;
             if (child != currentSize && array[child+1].compareTo(array[child])<0){
                 child++;
             }
-            if (array[child].compareTo(tmp)<0){
+            if (array[child].compareTo(t)<0){
                 array[hole] = array[child];
-            }else
+            }else {
                 break;
+            }
         }
-        array[hole] = tmp;
+        array[hole] = t;
     }
 
 
     private boolean isEmpty() {
         return currentSize == 0;
     }
+
+    private void enlargeArray(int size) {
+        T[] old = array;
+        array = (T[]) new Comparable[size];
+        for (int i = 0 ; i < old.length; i++){
+            array[i] = old[i];
+        }
+    }
+
 
     public static void main( String [ ] args )
     {
---
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_35/Sort.java
@@ -0,0 +1,92 @@
+package ink.lichen.basic.structure.test.test_35;
+
+import static ink.lichen.basic.structure.Support.CUT_OFF;
+import static ink.lichen.basic.structure.Support.swap;
+
+/**
+ * Created by lichen@daojia.com on 2019-2-14.
+ */
+public class Sort {
+
+    private static final int leftChild(int i){
+        return 2*i+1;
+    }
+
+    private static <T extends Comparable<? super T>> void heapSort(T[] a){
+        for (int i = a.length/2-1; i>= 0; i--){
+            percDown(a,i,a.length);
+        }
+
+        for (int i = a.length-1; i > 0 ; i--){
+            swap(a,0,i);
+            percDown(a,0,i);
+        }
+    }
+
+
+    private static <T extends Comparable<? super T>> void percDown(T[] a, int i, int n) {
+        int child;
+        T tmp = a[i];
+        for (; leftChild(i) < n; i = child){
+            child = leftChild(i);
+            if (child != n-1 && a[child+1].compareTo(a[child])>0){
+                child++;
+            }
+            if (a[child].compareTo(tmp)>0){
+                a[i] = a[child];
+            }else {
+                break;
+            }
+        }
+        a[i] = tmp;
+    }
+
+    private static <T extends Comparable<? super T>> void quickSort(T[] a){
+        quickSort(a,0,a.length-1);
+    }
+
+    private static <T extends Comparable<? super T>> void quickSort(T[] a,int left, int right){
+        if (left + CUT_OFF <= right){
+            T pivot = median3(a,left,right);
+            int i = left,j = right-1;
+            for (;;){
+                while (a[++i].compareTo(pivot)<0);
+                while (a[--i].compareTo(pivot)>0);
+                if (i < j){
+                    swap(a,i,j);
+                }else {
+                    break;
+                }
+            }
+
+            swap(a,i,right-1);
+            quickSort(a,left,i-1);
+            quickSort(a,i+1,right);
+        }else {
+            insertionSort(a,left,right);
+        }
+    }
+
+    private static <T extends Comparable<? super T>> T median3(T[] a, int left, int right) {
+        int center = (left + right) >> 1;
+        if (a[center].compareTo(a[left]) < 0){
+            swap(a,center,left);
+        }
+        if (a[right].compareTo(a[left]) < 0){
+            swap(a,right,left);
+        }
+        if (a[right].compareTo(a[center]) < 0){
+            swap(a,center,right);
+        }
+        swap(a,center,right-1);
+        return a[right-1];
+    }
+
+    private static <T extends Comparable<? super T>> void mergeSort(T[] a){
+
+    }
+
+    private static <T extends Comparable<? super T>> void insertionSort(T[] a,int left,int right){
+
+    }
+}
