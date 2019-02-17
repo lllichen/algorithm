a2399dd908bf5d49d449e027e399173494f02080
Li Chen <lichen@teleinfo.cn>
2019-02-17 23:08:18 +0800

    add code

---
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_35/RedBlackTree.java
@@ -0,0 +1,72 @@
+package ink.lichen.basic.structure.test.test_35;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-26.
+ */
+public class RedBlackTree<T extends Comparable<? super T>> {
+
+    private static final int BLACK = 1;
+    private static final int RED = 0;
+
+    private RedBlackNode<T> header;
+    private RedBlackNode<T> nullNode;
+
+    private RedBlackNode<T> current;
+    private RedBlackNode<T> parent;
+    private RedBlackNode<T> grand;
+    private RedBlackNode<T> great;
+
+    public RedBlackTree( ) {
+        nullNode      = new RedBlackNode<>(null);
+        nullNode.left = nullNode.right = nullNode;
+        header        = new RedBlackNode<>(null);
+        header.left   = header.right = nullNode;
+    }
+
+    private int compare(T item, RedBlackNode<T> t){
+        if (t == header){
+            return 1;
+        }else
+            return item.compareTo(t.element);
+    }
+
+    public void insert(T item){
+        current = parent = grand = great = header;
+        nullNode.element = item;
+        while (compare(item,current) != 0){
+            great = grand;grand = parent;parent = current;
+            current = compare(item,current) <0 ? current.left : current.right;
+
+            //check color;
+            if (current.left.color == RED && current.right.color == RED){
+                handleReorient(item);
+            }
+        }
+    }
+
+    private void handleReorient(T item) {
+
+    }
+
+    private static class RedBlackNode<T> {
+
+
+        public RedBlackNode(T theElement){
+            this(theElement,null,null);
+        }
+
+        T element;
+
+        RedBlackNode<T> left;
+        RedBlackNode<T> right;
+
+        int color;
+
+        public RedBlackNode(T theElement, RedBlackNode lt, RedBlackNode rt) {
+            element = theElement;
+            left = lt;
+            right = rt;
+            color = BLACK;
+        }
+    }
+}
---
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_36/BinaryHeap.java
@@ -0,0 +1,92 @@
+package ink.lichen.basic.structure.test.test_36;
+
+import ink.lichen.basic.structure.UnderflowException;
+
+public class BinaryHeap<T extends Comparable<? super T>> {
+
+    private static final int DEFAULT_SIZE = 10;
+
+    private T[] array;
+
+    private int currentSize;
+
+    public BinaryHeap(){
+        this(DEFAULT_SIZE);
+    }
+
+    public BinaryHeap(int size) {
+        currentSize = 0;
+        array = (T[]) new Comparable[size];
+    }
+
+    public void insert(T t){
+        if (currentSize == array.length-1){
+            enlargeArray(array.length*2+1);
+        }
+        int hole  = ++currentSize;
+        for (array[0] = t; t.compareTo(array[hole>>1]) < 0; hole >>= 1 ){
+            array[hole] = array[hole>>1];
+        }
+        array[hole] = t;
+    }
+
+    public T findMin(){
+        if (isEmpty()){
+            throw new UnderflowException();
+        }
+        T min = array[1];
+        return min;
+    }
+
+    public T deleteMin(){
+        if (isEmpty()){
+            throw new UnderflowException();
+        }
+        T min = findMin();
+        array[1] = array[currentSize--];
+        percolateDown(1);
+        return min;
+    }
+
+    private void percolateDown(int hole) {
+        int child;
+        T tmp = array[hole];
+        for (;hole << 1 <= currentSize; hole = child){
+            child = hole <<1;
+            if ( child != currentSize && array[child+1].compareTo(array[child])<0){
+                child++;
+            }
+            if (array[child].compareTo(tmp)<0){
+                array[hole] = array[child];
+            }else {
+                break;
+            }
+        }
+        array[hole] = tmp;
+    }
+
+    private void enlargeArray(int size) {
+        T[] old = array;
+        array = (T[]) new Comparable[size];
+        for (int i = 0 ; i < array.length; i++){
+            array[i] = old[i];
+        }
+    }
+
+    public boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+    public static void main( String [ ] args )
+    {
+        int numItems = 10000;
+        ink.lichen.basic.structure.test.test_35.BinaryHeap<Integer> h = new ink.lichen.basic.structure.test.test_35.BinaryHeap<>( );
+        int i = 37;
+
+        for( i = 37; i != 0; i = ( i + 37 ) % numItems )
+            h.insert( i );
+        for( i = 1; i < numItems; i++ )
+            if( h.deleteMin( ) != i )
+                System.out.println( "Oops! " + i );
+    }
+}
