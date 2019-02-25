652704b59d37485de27bff36d3119d297cc49574
lichen <lichen@daojia-inc.com>
2019-02-25 14:05:17 +0800

    add sort

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_37/Sort.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_37/Sort.java
@@ -1,11 +1,27 @@
 package ink.lichen.basic.structure.test.test_37;
 
+import static ink.lichen.basic.structure.Support.swap;
+
 /**
  * Created by lichen@daojia.com on 2019-2-25.
  */
 public class Sort {
 
+    private static final int leftChild(int i){
+        return 2*i+1;
+    }
+
     private static<T extends Comparable<? super T>>  void heapSort(T[] a){
+        for (int i = a.length/2-1; i>= 0;i--){
+            percDown(a,i,a.length);
+        }
+        for (int i = a.length - 1; i > 0; i--){
+            swap(a,0,i);
+            percDown(a,i,a.length);
+        }
+    }
+
+    private static void percDown(T[] a, int i, int n) {
 
     }
 }
