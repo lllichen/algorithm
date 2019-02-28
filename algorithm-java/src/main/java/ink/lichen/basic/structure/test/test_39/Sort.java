3c4a2bea5ca723796c8e25da0da0914ced9fb3f4
lichen <lichen@daojia-inc.com>
2019-02-28 17:25:07 +0800

    add info

---
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_39/Sort.java
@@ -0,0 +1,32 @@
+package ink.lichen.basic.structure.test.test_39;
+
+/**
+ * Created by lichen@daojia.com on 2019-2-28.
+ */
+public class Sort {
+
+
+    private static final   int leftChild(int i){
+        return (2<<1)+1;
+    }
+
+    private static <T extends Comparable<? super T>> void heapSort(T[] a){
+
+    }
+
+    private static <T extends Comparable<? super T>> void percDown(T[] a,int i,int n){
+
+    }
+
+    private static <T extends Comparable<? super T>> void quickSort(T[] a){
+
+    }
+
+    private static <T extends Comparable<? super T>> void mergeSort(T[] a){
+
+    }
+
+    private static <T extends Comparable<? super T>> void insertionSort(T[] a,int left, int right){
+
+    }
+}
