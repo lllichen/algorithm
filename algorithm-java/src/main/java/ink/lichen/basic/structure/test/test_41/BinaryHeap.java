45d23696ced1af24d4ea584d574683dfd509cd09
lichen <lichen@daojia-inc.com>
2019-03-12 10:28:28 +0800

    add info

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/BinaryHeap.java
@@ -3,5 +3,10 @@ package ink.lichen.basic.structure.test.test_41;
 /**
  * Created by lichen@daojia.com on 2019-3-8.
  */
-public class BinaryHeap {
+public class BinaryHeap<T extends Comparable<? super T>> {
+
+        private T[] array;
+
+//        private
+
 }
