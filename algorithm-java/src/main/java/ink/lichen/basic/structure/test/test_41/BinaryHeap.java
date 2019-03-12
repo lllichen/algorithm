7cff516eff9d121cbd97f4414b7a4b67a561052f
lichen <lichen@daojia-inc.com>
2019-03-12 16:34:52 +0800

    add info

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/BinaryHeap.java
@@ -19,4 +19,18 @@ public class BinaryHeap<T extends Comparable<? super T>> {
         currentSize = 0;
         array = (T[]) new Comparable[capacity];
     }
+
+    public void insert(T t){
+        if (currentSize == array.length-1){
+            enlargeArray(array.length*2+1);
+        }
+    }
+
+    private void enlargeArray(int size) {
+        T[] old = array;
+        array = (T[]) new Comparable[size];
+        for (int i = 0 ; i < old.length; i++){
+            array[i] = old[i];
+        }
+    }
 }
