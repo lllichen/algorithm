1353b18bad0573b726b92f4865f51033955e60ab
lichen <lichen@daojia-inc.com>
2019-03-12 16:51:10 +0800

    add info

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/BinaryHeap.java
@@ -1,5 +1,7 @@
 package ink.lichen.basic.structure.test.test_41;
 
+import ink.lichen.basic.structure.UnderflowException;
+
 /**
  * Created by lichen@daojia.com on 2019-3-8.
  */
@@ -24,6 +26,39 @@ public class BinaryHeap<T extends Comparable<? super T>> {
         if (currentSize == array.length-1){
             enlargeArray(array.length*2+1);
         }
+        int hole = ++currentSize;
+        for (array[0] = t;t.compareTo(array[hole>>1])<0;hole >>=1){
+            array[hole] = array[hole>>1];
+        }
+        array[hole] = t;
+    }
+
+    public T findMin(){
+        if (isEmpty()){
+            throw new UnderflowException();
+        }
+        T min = array[1];
+        return min;
+    }
+
+    public T deleteMin(){
+        if (isEmpty()){
+            throw new UnderflowException();
+        }
+        T min = findMin();
+        array[1] = array[currentSize--];
+        percolateDown(1);
+        return min;
+    }
+
+    private void percolateDown(int hole) {
+        int child;
+        T tmp = array[hole];
+//        for (int i = 0 ;i < )
+    }
+
+    private boolean isEmpty() {
+        return currentSize == 0;
     }
 
     private void enlargeArray(int size) {
