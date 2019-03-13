93c74fc5fa523f995c11b48c3c9e8a32e4b72314
lichen <lichen@daojia-inc.com>
2019-03-13 18:43:07 +0800

    add file

--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/BinaryHeap.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/BinaryHeap.java
@@ -54,7 +54,18 @@ public class BinaryHeap<T extends Comparable<? super T>> {
     private void percolateDown(int hole) {
         int child;
         T tmp = array[hole];
-//        for (int i = 0 ;i < )
+        for ( ; hole<<1 <= currentSize ;hole = child ){
+            child = hole <<1;
+            if (child != currentSize && array[child+1].compareTo(array[child])<0){
+                child++;
+            }
+            if (array[child].compareTo(tmp)<0){
+                array[hole] = array[child];
+            }else {
+                break;
+            }
+        }
+        array[hole] = tmp;
     }
 
     private boolean isEmpty() {
@@ -67,5 +78,18 @@ public class BinaryHeap<T extends Comparable<? super T>> {
         for (int i = 0 ; i < old.length; i++){
             array[i] = old[i];
         }
+    }
+
+    public static void main( String [ ] args )
+    {
+        int numItems = 10000;
+        BinaryHeap<Integer> h = new BinaryHeap<>( );
+        int i = 37;
+
+        for( i = 37; i != 0; i = ( i + 37 ) % numItems )
+            h.insert( i );
+        for( i = 1; i < numItems; i++ )
+            if( h.deleteMin( ) != i )
+                System.out.println( "Oops! " + i );
     }
 }
--- algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/Sort.java
+++ algorithm-java/src/main/java/ink/lichen/basic/structure/test/test_41/Sort.java
@@ -4,4 +4,26 @@ package ink.lichen.basic.structure.test.test_41;
  * Created by lichen@daojia.com on 2019-3-8.
  */
 public class Sort {
+
+    private static final int leftChild(int i){
+        return 2*i+1;
+    }
+
+    private static <T extends Comparable<? super T>> void heapSort(T[] a){
+
+    }
+
+    private static <T extends Comparable<? super T>> void quickSort(T[] a){
+
+    }
+
+    private static <T extends Comparable<? super T>> void mergeSort(T[] a){
+
+    }
+
+    private static <T extends Comparable<? super T>> void insertionSort(T[] a){
+
+    }
+
+
 }
